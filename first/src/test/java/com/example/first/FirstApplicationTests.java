8e2934e294f3a117fbc2220c86b758c4d0e79e8f
Benoît Fonty <benoit.fonty@myscript.com>
2020-12-01 14:28:41 +0100

    some password encoders

--- first/src/test/java/com/example/first/FirstApplicationTests.java
+++ first/src/test/java/com/example/first/FirstApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.first;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @SpringBootTest
 class FirstApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testPasswordEncoders() {
+		System.out.println(new BCryptPasswordEncoder().encode ("tatapouet"));
+		System.out.println(new Pbkdf2PasswordEncoder().encode ("tatapouet"));
+		System.out.println(new SCryptPasswordEncoder().encode ("tatapouet"));
 	}
 
 }
